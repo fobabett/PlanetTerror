ee308152242f9614cf191e109a69b902249f4db0
Te Vallee <astronomemusic@gmail.com>
2017-08-05 17:46:31 -1000

    enemy follows player

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Transform player;
+	public Transform myTransform;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (player);
+		transform.Translate(Vector3.forward*5*Time.deltaTime);
+	}
+}
